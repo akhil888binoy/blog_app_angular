f02cf02f5e3f4732acdf71978ef122e7e507b9e1
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2022-11-12 22:21:22 +0530

    blog app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home", component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
