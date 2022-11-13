b4c87f6a9e336bf51cd9412a45d23e68e631c06f
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2022-11-13 10:57:18 +0530

    side nav bar html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { SidebarComponent } from './admin/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
