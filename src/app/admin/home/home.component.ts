6ad5cee89873521647cbc87d51cc2dd1fc21b5ab
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2022-11-14 21:26:42 +0530

    breakpoint

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {BreakpointObserver} from '@angular/cdk/layout'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private observer: BreakpointObserver) { }
 
   ngOnInit(): void {
   }
