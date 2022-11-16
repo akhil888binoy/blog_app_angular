3f59145dd484e823088706ddf66cb1384d89e0e1
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2022-11-16 15:21:18 +0530

    date and time added

--- src/app/admin/dashboard/posts/postlist/postlist.component.ts
+++ src/app/admin/dashboard/posts/postlist/postlist.component.ts
@@ -1,26 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { post } from './post.model';
 import { PostsService} from './posts.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-postlist',
   templateUrl: './postlist.component.html',
   styleUrls: ['./postlist.component.sass']
 })
-export class PostlistComponent implements OnInit {
+export class PostlistComponent implements OnInit , OnDestroy{
   posts: post[]= []
-  /*posts=[
-    {title: 'Angular Blog by Akhil', content:'Blog app for writing blog'},
-    {title: 'Angular 14Blog by Akhil', content:'Blog app 14 for writing blog'},
-    {title: 'Angular Blog by Akhil from scratch', content:'Blog app from scratch for writing blog'},
-    {title: 'Angular Blog by Akhil MEAN stack', content:'Blog app for writing blog MEAN stack'}
-
-  ]*/
+  private postSub: Subscription;
 
   constructor( public ps: PostsService) { }
 
   ngOnInit(): void {
     this.posts =this.ps.getAll();
+    this.postSub =this.ps.getUpdateListener().subscribe((posts:post[])=>{
+      this.posts=posts;
+    })
   }
+  ngOnDestroy() {
+    this.postSub.unsubscribe();
 
+  }
 }
--- src/app/admin/dashboard/posts/postlist/posts.service.ts
+++ src/app/admin/dashboard/posts/postlist/posts.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { post } from './post.model';
-
+import {Subject} from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
   private posts:post[] =[];
+  private postUpdate= new Subject<post[]>();
 
+  getUpdateListener(){
+    return this.postUpdate.asObservable();
+  }
   constructor() { }
   addPost(title: string, content: string, dateTime: any){
     const post: post={title: title, content: content, dateTime: new Date() };
-    this.posts.push(post)
+    this.posts.push(post);
+    this.postUpdate.next([...this.posts]);
   }
   getAll(){
-    return this.posts;
+    return [...this.posts];
   }
 }
